0956dfe32e6c0341431c2382821242efdca51fbe
DESKTOP-LD289E6\julia <d2julian@gmail.com>
2022-06-17 23:07:24 +0200

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
         "booking-button": "#0071c2",
         "booking-button-hover": "#00487a",
         "booking-border": "#febb02",
+        "booking-dark": "#00224f",
       },
       width: {
         "booking-1024": "1024px",
       },
       borderWidth: {
         "1sm": "1px",
+      },
+      fontSize: {
+        xxs: "10px",
       },
     },
   },
